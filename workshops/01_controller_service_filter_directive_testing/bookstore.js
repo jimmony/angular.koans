886a0721cb862906cc878e0054bb7bd966c87cb6
Dries Schelfhout <dries.schelfhout@gmail.com>
2016-01-27 15:54:30 +0100

    Second commit

--- workshops/01_controller_service_filter_directive_testing/bookstore.js
+++ workshops/01_controller_service_filter_directive_testing/bookstore.js
@@ -17,27 +17,39 @@
   .controller('NavController', NavController);
 
   function RouteConfig($routeProvider) {
-  $routeProvider
-   .when('/', {
-    templateUrl: 'templates/book.html',
-    controller: 'BookController',
-  })
-  .when('/add', {
-    templateUrl: 'templates/add-book.html',
-    controller: 'BookController'
-  })
-  .when('/book/:id', {
-    templateUrl: 'templates/book-details.html',
-    controller: 'BookController'
-  });
-
-
-
+    $routeProvider
+      .when('/', {
+        templateUrl: 'templates/book.html',
+        controller: 'BookController',
+      })
+      .when('/books/add', {
+        templateUrl: 'templates/add-book.html',
+        controller: 'BookController'
+      })
+      .when('/books/:id', {
+        templateUrl: 'templates/book-details.html',
+        controller: 'BookController'
+      });
   }
 
-  function BookController($scope, BookService, BookFactory) {
-    console.log(BookService.getBook(9809));
-    $scope.bookList = BookService.getBooks();
+  function BookController($scope, $routeParams, BookService, BookFactory) {
+
+    if ($routeParams.id) {
+      $scope.book = BookFactory.getBook($routeParams.id);
+    } else {
+      $scope.book = {
+        covers: ['assets/img/angularjs.jpg', 'assets/img/coman.jpeg'],
+        add: function () {
+          debugger
+          addBook();
+        }
+      }
+    }
+    function addBook () {
+
+          BookFactory.addBook($scope.book);
+        }
+    $scope.bookList = BookFactory.getBooks();
   }
 
 
@@ -47,7 +59,7 @@
 
   function BookServiceFn() {
     this.getBook = function (id) {
-      var found = books.filter((book) => book.bookID === id);
+      var found = books.filter((book) => book.bookID == id);
       if (found[0]) {
         return found[0]
       }
@@ -57,7 +69,7 @@
       return books
     };
     this.deleteBook = function (id) {
-      var index = books.findIndex((book) => book.bookID === id);
+      var index = books.findIndex((book) => book.bookID == id);
       if (index !== -1) {
         books.splice(index, 1);
       }
@@ -65,21 +77,48 @@
   }
 
   function BookFactoryFn() {
-    return {
-      getBook : function(id) {
-        var found = books.filter((book) => book.bookID === id);
-        if (found[0]) {
-          return found[0]
-        }
-        return null;
-      },
-      getBooks : function() {return books},
-      deleteBook : function(id) {
-        var index = books.findIndex((book) => book.bookID === id);
-        if (index !== -1) {
-          books.splice(index, 1);
-        }
+    function getBook(id) {
+      var found = books.filter((book) => book.bookID == id);
+      if (found[0]) {
+        return found[0]
       }
+      return null;
+    }
+
+    function getBooks() {
+      return books;
+    }
+
+    function addBook(book) {
+      book.bookID = nextBookId();
+      delete book.covers;
+      getBooks().push(book);
+    }
+
+    function deleteBook(id) {
+      var index = books.findIndex((book) => book.bookID == id);
+      if (index !== -1) {
+        books.splice(index, 1);
+      }
+    }
+
+    function nextBookId() {
+      var random = getRandomIntInclusive(0, 10000);
+      while (getBook(random) != null) {
+        random = angular.random(10000);
+      }
+      return random;
+    }
+
+    function getRandomIntInclusive(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    return {
+      getBook : getBook,
+      getBooks : getBooks,
+      addBook : addBook,
+      deleteBook : deleteBook
     };
   }
 
